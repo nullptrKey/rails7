c2a603ae80f25421d45961b198299ba6ba452bd7
nullptrKey <1027714618@qq.com>
2025-05-08 22:52:36 +0800

    add index style

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :articles
+  get "home/index"
   get "articles/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
+  root "home#index"
   # Defines the root path route ("/")
   # root "posts#index"
 end
