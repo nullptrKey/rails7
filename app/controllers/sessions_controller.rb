9d588c7c67f24ef8c2df5263d8b4eb013ace326d
NullCode <NullCode@qq.com>
2025-05-18 17:24:19 +0800

    add login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+class SessionsController <  ApplicationController
+  def new
+
+  end
+
+  def create
+    #Rails.logger.info "Params: #{params.inspect}"
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "You have successfully logged in"
+      redirect_to user_path(user)
+    else
+
+      flash.now[:alert] = "There was something wrong with your login information"
+      render 'new'
+
+    end
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You have logged out"
+    redirect_to root_path
+  end
+
+  end
+
+end
